418e6d83b61a7b47024470b302beea8c951fc4fa
jashimgit <jashim.ncn@gmail.com>
2021-03-10 03:04:11 +0600

    useEffect hook added with api link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,34 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './Home.css';
+import SingleTeam from './../SingleTeam/SingleTeam';
 const Home = () => {
+    const [allTeams, setAllTeams] = useState({});
+    // check fetch data 
+    useEffect(() => {
+        const url = 'https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllTeams(data));
+    });
+
+    // console.log(allTeams.teams[0]);
+    allTeams.teams[0].map(t => console.log(t.id))
+
+
     return (
         <div>
-            <h2>Welcome to Quick BD TEAM</h2>
+            <header>
+                <h3>Quick BD TEAM</h3>
+            </header>
+            <section className="team-section container mt-4">
+                <div className="row">
+                    {/* {
+                        allTeams.map(team => <SingleTeam singleData={team}></SingleTeam>)
+                    } */}
+
+
+                </div>
+            </section>
         </div>
     );
 };
