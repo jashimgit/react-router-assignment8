ee09977c64403f2213924ac1c9196f67401d06e9
jashimgit <jashim.ncn@gmail.com>
2021-03-10 19:19:23 +0600

    dynamic conditon added for team details image for male or female

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Quick BD TEAM
 
-Quick BD TEAM is a learning project build with React JS
+Welcome to Quick BD TEAM web app which is a learning project build with React JS.
+
+
+## Supported content I Used
+1. thesportsdb.com/api.php 
+2. Font Awesome Icon
+3. Bootstrap 
+
+## Features
+This is a simple app where you will see some of the Teams with their team logo.
+    1. List all team by leauge
+    2. Show details of team after button clicked
+
+## Technological Feature I used
+I used here React Router dom for routing though I didn't use any navigation bar to access the url
+I used useState(), useEffect(), useParams() hook.
+All Team information is fetched from thesportsdb API, here i use two different API link to get my data from API
+
+
+Thanks to thesportsdb, Netlify and Programming Hero Team and My instructor Mr. Jhankar Mahbub.
+
--- src/components/TeamDetails/TeamDetails.js
+++ src/components/TeamDetails/TeamDetails.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { coffee } from '@fortawesome/free-solid-svg-icons';
-
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { faMars } from '@fortawesome/free-solid-svg-icons';
+import { faFutbol } from '@fortawesome/free-solid-svg-icons';
+import { faFlag } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import './teamdetails.css';
+import maleImage from '../../assets/team-photo/male.png';
+import femaleImage from '../../assets/team-photo/female.png';
 
 const TeamDetails = () => {
     const { teamId } = useParams();
+    const [teamdata, setTeamdata] = useState({ teamId });
+    let teamImage;
 
-    const [teamdata, setTeamdata] = useState({});
 
     useEffect(() => {
-        const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setTeamdata(data.teams))
-    })
+        if (teamId) {
+            const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}`;
+            fetch(url)
+                .then(res => res.json())
+                .then(data => setTeamdata(data.teams))
+        }
+
+    }, [teamId])
     // console.log(teamdata.strDescriptionEN)
+
+    // check for male or female team 
+    if (teamdata[0]?.strGender === "Male") {
+        teamImage = maleImage;
+    } else if (teamdata[0]?.strGender === "Female") {
+        teamImage = femaleImage;
+    }
+
+
     return (
         <div>
             <header>
@@ -29,35 +50,43 @@ const TeamDetails = () => {
                                 <div className="col-md-8">
                                     <div className="card-body">
                                         <h5 className="card-title">{teamdata[0]?.strTeam}</h5>
-                                        <p clasName="card-text">Founded:{teamdata[0]?.intFormedYear}</p>
-                                        <p className="card-text">Country name : {teamdata[0]?.strCountry}</p>
-                                        <p className="card-text">Sport Type : {teamdata[0]?.strSport}</p>
-                                        <p className="card-text">Gender Male/ Female</p>
+                                        <p clasName="card-text"> <FontAwesomeIcon icon={faMapMarkerAlt} /> Founded : {teamdata[0]?.intFormedYear}</p>
+                                        <p className="card-text"><FontAwesomeIcon icon={faFlag} /> Country name : {teamdata[0]?.strCountry}</p>
+                                        <p className="card-text"> <FontAwesomeIcon icon={faFutbol} /> Sport Type : {teamdata[0]?.strSport}</p>
+                                        <p className="card-text"><FontAwesomeIcon icon={faMars} /> Gender : {teamdata[0]?.strGender}</p>
 
                                     </div>
                                 </div>
                                 <div className="col-md-4 p-4">
-                                    <img src="saf" alt="..." />
+                                    <img className="img-fluid w-100" src={teamImage} alt="..." />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-sm-12 mt-3 text-white">
-                    <p>{teamdata[0]?.strDescriptionEN}</p>
+                    <p className="text-justify">{teamdata[0]?.strDescriptionEN}</p>
                 </div>
+
+
                 <footer className="col-sm-12 mt-4">
                     <div className="social-icon d-flex justify-content-center text-white">
 
-
-                        <span><FontAwesomeIcon icon={["far", "coffee"]} /></span>
+                        <span>
+                            <a href={teamdata[0]?.strFacebook} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
+                        </span>
+                        <span>
+                            <a href={teamdata[0]?.strTwitter} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} /></a>
+                        </span>
+                        <span>
+                            <a href="instagram.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+                        </span>
 
 
                     </div>
                 </footer>
             </div>
-            <p>{teamId}</p>
-        </div>
+        </div >
     );
 };
 
