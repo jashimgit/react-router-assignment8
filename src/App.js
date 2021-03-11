df1071f012a1d3a2642dd4d400c216d6108edc3d
jashimgit <jashim.ncn@gmail.com>
2021-03-11 20:35:45 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home/Home';
 import Nomatch from './components/Nomatch/Nomatch';
 import TeamDetails from './components/TeamDetails/TeamDetails';
+import Nav from './components/navbar/nav';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Nav></Nav>
         <Switch>
           <Route path="/team/:teamId">
             <TeamDetails></TeamDetails>
---
+++ src/components/navbar/nav.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+const Nav = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark bg-dark myNavColor' >
+            <div className='container'>
+                <a className='navbar-brand' href='/'>
+                    Quick Team BD
+				</a>
+                <button
+                    className='navbar-toggler'
+                    type='button'
+                    data-toggle='collapse'
+                    data-target='#navbarSupportedContent'
+                    aria-controls='navbarSupportedContent'
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div
+                    className='collapse navbar-collapse'
+                    id='navbarSupportedContent'
+                >
+                    <ul className='navbar-nav ml-auto'>
+                        <li className='nav-item'>
+                            <Link className='nav-link' to='/'>
+                                Home
+							</Link>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
